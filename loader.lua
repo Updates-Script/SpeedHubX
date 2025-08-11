0921e9051975aa5e3bf2e8de11485261daeea0e9
Updates-Script <cutenies123@gmail.com>
2025-08-11 17:43:18 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,12 +1,44 @@
--- Show notification about the key
-game.StarterGui:SetCore("SendNotification", {
-    Title = "Speed Hub X";
-    Text = "Please wait... You will need a key to proceed.";
-    Duration = 5; -- seconds
-})
+-- Create a ScreenGui
+local screenGui = Instance.new("ScreenGui")
+screenGui.IgnoreGuiInset = true
+screenGui.ResetOnSpawn = false
+screenGui.Parent = game.CoreGui
 
--- Wait before loading the main script
-task.wait(2)
+-- Background frame
+local frame = Instance.new("Frame")
+frame.Size = UDim2.new(1, 0, 1, 0)
+frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+frame.BackgroundTransparency = 0.3
+frame.Parent = screenGui
 
--- Load the actual Speed Hub X script
-loadstring(game:HttpGet("https://rawscripts.net/raw/a-literal-baseplate.-Speedhub-X-30971"))()
+-- Centered message
+local textLabel = Instance.new("TextLabel")
+textLabel.Size = UDim2.new(1, 0, 1, 0)
+textLabel.BackgroundTransparency = 1
+textLabel.Text = "Please wait while getting key..."
+textLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+textLabel.TextStrokeTransparency = 0
+textLabel.Font = Enum.Font.SourceSansBold
+textLabel.TextSize = 40
+textLabel.Parent = frame
+
+-- Function to detect the key UI
+local function waitForKeyUI()
+    while true do
+        task.wait(0.2)
+        if game.CoreGui:FindFirstChild("SpeedHubX") then -- change name if UI has a different name
+            break
+        end
+    end
+end
+
+-- Run the loader script in the background
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Updates-Script/SpeedHubX/refs/heads/main/loader.lua"))()
+end)
+
+-- Wait until key UI is detected
+waitForKeyUI()
+
+-- Remove the big notification
+screenGui:Destroy()
