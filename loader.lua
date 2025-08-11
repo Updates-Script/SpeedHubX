b45e769480328823b10e8aed31450dc3a5f8eee4
Updates-Script <cutenies123@gmail.com>
2025-08-11 17:36:05 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,12 @@
+-- Show notification about the key
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Speed Hub X";
+    Text = "Please wait... You will need a key to proceed.";
+    Duration = 5; -- seconds
+})
+
+-- Wait before loading the main script
+task.wait(2)
+
+-- Load the actual Speed Hub X script
 loadstring(game:HttpGet("https://rawscripts.net/raw/a-literal-baseplate.-Speedhub-X-30971"))()
